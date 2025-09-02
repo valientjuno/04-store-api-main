2d36cd8ab0fb9ecd8f02e494fbc72caec3c34cf2
valientjuno <jessedoake91@gmail.com>
2025-09-02 13:51:32 -0600

    commitment 5
    commitment not working . needs code to be fixed. video around 4:35:19

--- controllers/products.js
+++ controllers/products.js
@@ -1,30 +1,41 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "ab";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Product.find({}).select("name price").limit(4);
   res.status(200).json({ products, nbHits: products.length });
 };
+
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  if (company) {
-    queryObject.company = company;
-  }
-  if (name) {
-    queryObject.name = { $regex: name, $options: "i" };
-  }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
-  res.status(200).json({ products, nbHits: products.length });
 };
+if (company) {
+  queryObject.company = company;
+}
+// console.log(queryObject);
+let result = Product.find(queryObject).sort();
+{
+  // sort
 
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAT");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+}
 module.exports = {
   getAllProducts,
   getAllProductsStatic,
